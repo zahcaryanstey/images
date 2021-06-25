c721889ce703ad19807ff25c785ae91428e1c549
zahcaryanstey <77417058+zahcaryanstey@users.noreply.github.com>
2021-06-25 10:04:54 -0230

    Add files via upload
    Script to take the .ome.tif images in our data set and change the file name to .tif

---
+++ ChangeFileExtension.py
@@ -0,0 +1,26 @@
+# This is a script to change the file extension on our images
+# The original images have a file extension of .ome.tif
+# We need to change the extension so that the images
+# are now saved as .tiff
+
+
+
+
+
+# First we need to import the necessary libraries
+import os
+
+
+
+folder = '/home/zachary/Desktop/Test_images/cell_images'
+import os
+
+paths = (os.path.join(root, filename)
+        for root, _, filenames in os.walk(folder)
+        for filename in filenames)
+
+for path in paths:
+    # take the .ome.tif part of the file name and change to .tif
+    newname = path.replace('.ome.tif', '.tif')
+    if newname != path:
+        os.rename(path, newname)
